97fa4bc0ec0fb45c5e9355046533f1e5131020c1
tushar2704 <tushar.inseec@gmail.com>
2023-08-22 15:16:55 +0530

    Streamlit application 1 updates

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ st.markdown("""[Tushar-Aggarwal.com](https://tushar-aggarwal.com/)""")
 @st.cache_data(ttl=600)
 def load_data(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
-    return pd.read_csv(csv_url, encoding ='utf-8')
+    return pd.read_csv(csv_url, encoding ='utf-8',, skiprows=3)
 
 df = load_data(st.secrets["public_gsheets_url"])
 
